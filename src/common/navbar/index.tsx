e16f73fce5d1455c8dbe19b95861b03d0e355c38
Icaro Asas Virtuais <icaro.capobianco@gmail.com>
2023-08-15 21:17:27 -0300

    + nav box props

--- src/common/navbar/index.tsx
+++ src/common/navbar/index.tsx
@@ -3,7 +3,7 @@ import { Center, Container, HStack, Heading, Box, Link, BoxProps } from '@chakra
 import ColorModeButton from '../../chakra/color-mode-button'
 import Icon from '../icon'
 
-export default function Navbar( { children, ...props } : PropsWithChildren<BoxProps> ) {
+export function Navbar( { children, ...props } : PropsWithChildren<BoxProps> ) {
     return (
         <Box as='nav' {...props}>
             <Container maxW='container.xl'>
@@ -23,3 +23,5 @@ export default function Navbar( { children, ...props } : PropsWithChildren<BoxPr
         </Box>
     )
 }
+
+export default Navbar
